f86c917feb7284be133bcb8e32b2430768ace41f
MuhammadSaddamNur <muhammadsaddamnur@gmail.com>
2018-04-25 14:30:15 +0700

    tambah form pembayaran

---
+++ app/src/main/java/com/saddam/nur/kalimasadabaru/View/pembayaran.java
@@ -0,0 +1,15 @@
+package com.saddam.nur.kalimasadabaru.View;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.saddam.nur.kalimasadabaru.R;
+
+public class pembayaran extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pembayaran);
+    }
+}
